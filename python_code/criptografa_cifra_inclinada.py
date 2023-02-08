cc5d37bb709a195758f15b5873bb3791c66b46ff
Matteo K. F. Giuliani <67129306+matteokfg@users.noreply.github.com>
2023-02-08 10:26:53 -0300

    Update criptografa_cifra_inclinada.py
    Resolvida a Issue #1

--- python_code/criptografa_cifra_inclinada.py
+++ python_code/criptografa_cifra_inclinada.py
@@ -1,13 +1,22 @@
-def espacos_por_underline(String):
-    String = list(String)
-    for v, i in enumerate(String):
+def espacos_por_underline(string):
+    string = list(string)
+    for v, i in enumerate(string):
         if i == " ":
-            String[v] = "_"
-    return String
+            string[v] = "_"
+    return string
 
-def validate_String(String, numberOfRows):
-    if (numberOfRows >= 1 and numberOfRows <= 2*(10**3)) and (len(String) >= 1 and len(String) <= 2*(10**6)) and (len(String) % numberOfRows == 0):
-        if String.isalpha() or ' ' in String:
+def falsos_undelines_String(string, numberOfRows):
+    if (len(string) % numberOfRows == 0):
+        booleano = True
+    else:
+        while len(string) % numberOfRows != 0: # coloca underlines no final da string para que o tamanho fique divisivel pelo numero de linhas
+            string.append("_")
+        booleano = False
+    return string, booleano
+
+def validate_String(string, numberOfRows):
+    if (numberOfRows >= 1 and numberOfRows <= 2*(10**3)) and (len(string) >= 1 and len(string) <= 2*(10**6)):
+        if string.isalpha() or ' ' in string:
             return True
         else:
             return False
@@ -15,31 +24,36 @@ def validate_String(String, numberOfRows):
         print("Algo deu errado!")
         return False
 
-def encoding_String(String, numberOfRows):
-    #------------ crio a matriz --------------------------
-    n_colunas = int(len(String)/numberOfRows) + (numberOfRows - 1) # a quantidade de colunas vai ser o tamanho da string dividido pelo numero de linhas mais o numeros de linha menos 1
-    matriz = [["_" for i in range(n_colunas)] for i in range(numberOfRows)] # crio a matriz
+def encoding_String(string, numberOfRows):
+    #------------ preparo a string / vira uma lista -----------------------
+    new_String = espacos_por_underline(string)                                            # substituo o valor dos espacos por underlines
+    new_String, string_verdadeira = falsos_underlines_String(new_String, numberOfRows)    # garanto que a string tenha uma tamanho divisivel pelo numero de linhas, por se precisar colocar no final dela quantos underlines forem necessarios
+    copia_string = new_String.copy()                                                      # faco uma copia da lista da string ja que vou apagar o item inicial a cada passagem pelo looping
 
-    #------------ preparo a string -----------------------
-    new_String = espacos_por_underline(String) # substituo o valor os espacos por underlines
-    copia_string = new_String.copy() # faco uma copia da lista da string ja que vou apagar o item inicial a cada passagem pelo looping
+    #------------ crio a matriz --------------------------
+    if string_verdadeira:
+        n_colunas = int(len(new_String)/numberOfRows) + (numberOfRows - 1)                # a quantidade de colunas vai ser o tamanho da string dividido pelo numero de linhas mais o numeros de linha menos 1
+        matriz = [["_" for i in range(n_colunas)] for i in range(numberOfRows)]           # crio a matriz
+    else:
+        n_colunas = int(len(new_String)/numberOfRows) + (numberOfRows - 2)                # a quantidade de colunas vai ser o tamanho da string dividido pelo numero de linhas mais o numeros de linha menos 2, garanto que a coluna com apenas underlines falsos sea excluida
+        matriz = [["_" for i in range(n_colunas)] for i in range(numberOfRows)]           # crio a matriz
 
     #------------ popula a matriz ------------------------
     contador_linhas = range(numberOfRows)
     try:
-        for el in range(int((len(String)/numberOfRows))): # roda a quantidade de diagonais lidas, caso de erro fora de indice na ultima letra tem o try e except
-            for i in contador_linhas: # roda as X linhas
-                matriz[i][i+el] = copia_string[0] # coloca o caracter da string no seu lugar na matriz/diagonal
-                copia_string.pop(0) # para que n seja repetido, retira-se o item, assim dando o lugar para o proximo valor a ser colocado na prozima vez no for loop
+        for el in range(int((len(new_String)/numberOfRows))):                   # roda a quantidade de diagonais lidas, caso de erro fora de indice na ultima letra tem o try e except
+            for i in contador_linhas:                                           # roda as X linhas
+                matriz[i][i+el] = copia_string[0]                               # coloca o caracter da string no seu lugar na matriz/diagonal
+                copia_string.pop(0)                                             # para que n seja repetido, retira-se o item, assim dando o lugar para o proximo valor a ser colocado na prozima vez no for loop
     except:
         pass
 
-    #------------ le a matriz ----------------------------
+    #------------ le a matriz linha por linha ----------------------------
     for linha in matriz:
         for caracter in linha:
             print(caracter, end="")
     print()
 
-def encode_String(String, numberOfRows):
-    if validate_String(String, numberOfRows):
-        encoding_String(String, numberOfRows)
+def encode_String(string, numberOfRows):
+    if validate_String(string, numberOfRows):
+        encoding_String(string, numberOfRows)
