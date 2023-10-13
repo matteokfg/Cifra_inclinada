0f77a040e3e8afca0c01f8ebb0c276c138ad769f
Matteo K. F. Giuliani <67129306+matteokfg@users.noreply.github.com>
2023-10-13 19:32:37 -0300

    Added test of the returned values

--- elixir_code/cripto_test.exs
+++ elixir_code/cripto_test.exs
@@ -1,23 +1,35 @@
 defmodule CriptoTest do
   use ExUnit.Case
 
-  test "criptografia" do
-    IO.puts("TESTE Criptografia")
+  test "criptografia - frase pequena - valor" do
+    assert Cripto.criptografa("my name is", 3) == 'mnes__ya_____mi'
+  end
 
+  test "criptografia - frase media - valor" do
+    assert Cripto.criptografa("nao havera borboletas se a vida nao passar por longas e silenciosas metamorfoses", 4) == 'nhrols_inpaoossnsmmo____aaare_adaarrn_icaeos____ov_bts_aos__gelistre_____eboaev__spla_eo_afs'
+  end
+
+  test "criptografia - frase grande - valor" do
+    assert Cripto.criptografa("para mim as horas sao curtas e os dias pequenos por mais longas que sejam as horas e maiores que sejam dos dias nao sao suficientes para satisfazer a vontade que sinto de estar sempre com voce", 6) == 'pihsr_aupigemo_e__iosepazvesdapm______amoatoseosa__rmssda_unateo_ierr_______r_roas_nr_ssaaa_eossftrirnqn__ev______aaa_s_po_l_essiqjs_aieas_tutes_o_______ssc_desmoqj__oua_nocs_faaeosecc______m__ueiq_anuaheremda_i_sa_d__tmoe'
+  end
+
+  test "criptografia - frase pequena - tempo" do
     inicio = Time.utc_now()
     Cripto.criptografa("my name is", 3)
     fim = Time.utc_now()
     tempo = Time.diff(fim, inicio, :microsecond)
     IO.puts("#{tempo} microssegundos \n")
+  end
 
-
+  test "criptografia - frase media - tempo" do
     inicio = Time.utc_now()
     Cripto.criptografa("nao havera borboletas se a vida nao passar por longas e silenciosas metamorfoses", 4)
     fim = Time.utc_now()
     tempo = Time.diff(fim, inicio, :microsecond)
     IO.puts("#{tempo} microssegundos \n")
+  end
 
-
+  test "criptografia - frase grande - tempo" do
     inicio = Time.utc_now()
     Cripto.criptografa("para mim as horas sao curtas e os dias pequenos por mais longas que sejam as horas e maiores que sejam dos dias nao sao suficientes para satisfazer a vontade que sinto de estar sempre com voce", 6)
     fim = Time.utc_now()
